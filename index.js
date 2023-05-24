5e376b9cb267d85be0b9743e2800cd9ec8e5437f
EmmilyOdhiambo <emmilysodhiambo@gmail.com>
2023-05-24 17:25:53 +0300

    dom assignment

--- index.js
+++ index.js
@@ -13,7 +13,29 @@ for (let i = 0; i < h3Titles.length; i++){
 }
 
 //Add one more fruit to the fruit list
-let fruitList = document.getElementBy('fruList');
-let newFruit =document.createElement('li');
-newFruitList.textContent = "Berries";
-fruitList.appndChild(newFruit);
+// let fruitList = document.getElementById('fruList');
+let fruit =document.createElement('li');
+fruit.innerHTML = 'Berries'
+document.getElementById('fruList').appendChild(fruit);
+// fruitList.appendChild(newFruit);
+
+//Add one more vegetable to the vetable list
+// let vegetableList = document.getElementById('vegList');
+let vegetable = document.createElement('li');
+vegetable.innerHTML = 'Cabbage';
+vegList.appendChild(vegetable);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
